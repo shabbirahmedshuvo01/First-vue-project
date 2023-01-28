f43dfef06527edf59eff132889db129cf3d73a52
unknown <sabbirshuvo006@gmail.com>
2023-01-28 17:43:40 +0600

    sign up work in value

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,64 +1,88 @@
 <template>
   <h1 class="text-4xl font-bold">Sign Up</h1>
-  <img class="logo" src="./../assets/download.png" alt="" />
+  <!-- <img class="logo" src="./../assets/download.png" alt="" />
 
   <section>
     <div>
       <input type="text" placeholder="Type Your Name" />
     </div>
-  </section>
+  </section> -->
 
   <section class="mx-">
-    <div class="hero min-h-screen bg-base-200">
-      <div class="hero-content flex-col lg:flex-row-reverse">
-        <div class="text-center lg:text-left">
-          <h1 class="text-5xl font-bold">Login now!</h1>
-          <p class="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-        </div>
-        <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <div class="card-body">
-            <div class="form-control">
-              <label class="label">
-                <span class="label-text">Email</span>
-              </label>
-              <input
-                type="text"
-                placeholder="email"
-                class="input input-bordered"
-              />
-            </div>
-            <div class="form-control">
-              <label class="label">
-                <span class="label-text">Password</span>
-              </label>
-              <input
-                type="text"
-                placeholder="password"
-                class="input input-bordered"
-              />
-              <label class="label">
-                <a href="#" class="label-text-alt link link-hover"
-                  >Forgot password?</a
-                >
-              </label>
-            </div>
-            <div class="form-control mt-6">
-              <button class="btn btn-primary">Login</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <section class="h-full gradient-form bg-gray-200 md:h-screen">
+                <div class="container py-12 px-6 h-full">
+                    <div class="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+                        <div class="xl:w-10/12">
+                            <div class="block bg-white shadow-lg rounded-lg">
+                                <div class="lg:flex lg:flex-wrap g-0">
+                                    <div class="lg:w-6/12 px-4 md:px-0">
+                                        <div class="md:p-12 md:mx-6">
+                                            <div class="text-center">
+                                                <img
+                                                    class="mx-auto w-48"
+                                                    src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
+                                                    alt="logo"
+                                                />
+                                                <h4 class="text-xl font-semibold mt-1 mb-12 pb-1">We are The Lotus Team</h4>
+                                            </div>
+                                            <section>
+                                                <p class="mb-4">Please Create to your account</p>
+                                                <div class="mb-4">
+                                                    <input
+                                                        type="text" name="name" id="" v-model="name" placeholder='Your Name' required
+                                                        class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" />
+                                                </div>
+                                                <div class="mb-4">
+                                                    <input
+                                                        type="email" name="email" id="" v-model="email" placeholder='Email address' required
+                                                        class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" />
+                                                </div>
+                                                <div class="mb-4">
+                                                    <input
+                                                        type="password" name="password" id="" v-model="password" placeholder='Password' required
+                                                        class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" />
+                                                </div>
+                                                <div class="text-center pt-1 mb-12 pb-1">
+                                                    <!-- <input
+                                                        className='btn btn-success mt-2' type="submit" value="register" /> -->
+
+                                                        <button class="btn btn-success mt-2" v-on:click="signUp">submit</button>
+
+                                                    <br />
+                                                    <a class="text-gray-500 mt-5" href="#!">Forgot password?</a>
+                                                </div>
+                                                <div class="">
+                                                    <!-- <p class="mb-0 mr-2">Don't have an account? <span><Link class="text-blue-600" to={'/login'} >Sign Up</Link></span> </p> -->
+                                                </div>
+                                            </section>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
   </section>
 </template>
 
 <script>
 export default {
   name: "SignUp",
+  data()
+  {
+    return{
+        name:"",
+        email:"",
+        password:""
+    }
+  },
+  methods:{
+    signUp()
+    {
+        console.warn("signup", this.name,this.email,this.password)
+    }
+  }
 };
 </script>
 
